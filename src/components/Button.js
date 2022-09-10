6179a61745668e38c00e9d77d13eee5b5673f19a
Willyan Lauriente <willyanf17@hotmail.com>
2022-09-10 16:15:45 -0300

    added dymanic styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import tw from 'twrnc';
 
-export default function Button({ onPress, name }) {
+export default function Button({ onPress, name, color, width, textColor }) {
     return (
-        <TouchableOpacity style={tw`bg-white p-3 w-60 rounded-md shadow-md items-center`} onPress={onPress}>
-            <Text style={tw`font-bold text-slate-500`}>{name}</Text>
+        <TouchableOpacity style={tw`${color} p-3 ${width} rounded-md shadow-md items-center`} onPress={onPress}>
+            <Text style={tw`font-bold ${textColor}`}>{name}</Text>
         </TouchableOpacity>
     )
 }
