e7caa4116194d9b876cb162c07bb41c83025e1de
Willyan Lauriente <willyanf17@hotmail.com>
2022-09-10 17:09:27 -0300

    added screen weight bone

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import IMCScreen from './src/pages/IMCScreen';
 import HomeScreen from './src/pages/HomeScreen';
+import WeightBoneScreen from './src/pages/WeightBoneScreen';
 
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen options={{ headerTitleAlign: 'center' }} name="Home" component={HomeScreen} />
         <Stack.Screen options={{ headerTitleAlign: 'center' }} name="IMC" component={IMCScreen} />
+        <Stack.Screen options={{ headerTitleAlign: 'center', title: 'Peso Osseo' }} name="WeightBone" component={WeightBoneScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/pages/WeightBoneScreen.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { View, Text, TextInput } from "react-native";
+import tw from 'twrnc';
+import Button from "../components/Button";
+
+export default function WeightBoneScreen() {
+
+    const [height, setHeight] = useState(0);
+    const [fish, setFish] = useState(0)
+    const [knee, setKnee] = useState(0)
+    const [result, setResult] = useState(0)
+
+
+    const calcWeightBone = () => {
+        setResult(3.02 * (400 * fish * knee * (height * height)))
+        console.log(result)
+    }
+
+    return (
+        <View style={tw`w-full h-full bg-slate-100 items-center justify-center`}>
+            <View style={tw`flex flex-row`}>
+                <Text style={tw`text-slate-500 font-bold text-6 mb-10`}>Calculadora</Text>
+                <Text style={tw`italic font-bold text-6 text-green-400`}> Peso Ósseo</Text>
+            </View>
+            <TextInput
+                style={tw`bg-white rounded-md shadow-md p-2 w-60 mt-3`}
+                placeholder="Altura"
+                keyboardType="numeric"
+                value={height}
+                onChangeText={(text) => setHeight(text / 100)} />
+
+            <TextInput
+                style={tw`bg-white rounded-md shadow-md p-2 w-60 mt-3`}
+                placeholder="Diâmetro do Punho"
+                keyboardType="numeric"
+                value={fish}
+                onChangeText={(text) => setFish(text / 100)} />
+
+            <TextInput
+                style={tw`bg-white rounded-md shadow-md p-2 w-60 mt-3 mb-5`}
+                placeholder="Diâmetro do Joelho"
+                keyboardType="numeric"
+                value={knee}
+                onChangeText={(text) => setKnee(text / 100)} />
+
+            <Button onPress={() => calcWeightBone()} color={'bg-green-500'} textColor={'text-white'} width={'w-50'} name={"Calcular"} />
+
+            <Text style={tw`mt-5`}>Peso Ósseo: {result}</Text>
+        </View>
+    )
+}
