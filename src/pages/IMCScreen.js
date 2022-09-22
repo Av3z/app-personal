02b10dcb1140a319df798fcce650e12056287640
Willyan Lauriente <willyanf17@hotmail.com>
2022-09-22 20:25:24 -0300

    added situation in IMC

--- src/pages/IMCScreen.js
+++ src/pages/IMCScreen.js
@@ -4,12 +4,25 @@ import tw from 'twrnc'
 
 export default function IMCSCreen() {
 
-    const [weight, setWeight] = useState(0);
-    const [height, setHeight] = useState(0);
+    const [weight, setWeight] = useState();
+    const [height, setHeight] = useState();
+    const [result, setResult] = useState('Normal');
     const [imc, setImc] = useState(0);
 
     const calcImc = () => {
         setImc(weight / (height * height));
+
+        let imcResult = '';
+
+        if (imc < 18.5) imcResult = 'Abaixo do Peso';
+        else if (imc >= 18.5 && imc <= 24.9) imcResult = 'Normal';
+        else if (imc > 24.9 && imc <= 29.9) imcResult = 'Excesso de Peso';
+        else if (imc > 29.9 && imc <= 34.9) imcResult = 'Obesidade classe I';
+        else if (imc > 34.9 && imc <= 39.9) imcResult = 'Obesidade classe II';
+        else if (imc >= 40) imcResult = 'Obesidade classe III';
+        else console.log('Erro dados inválidos!')
+
+        setResult(imcResult);
     }
 
     return (
@@ -39,7 +52,8 @@ export default function IMCSCreen() {
             >
                 <Text style={tw`text-white font-bold`}>Calcular</Text>
             </TouchableOpacity>
-            <Text style={tw`mt-5`}>IMC: {imc.toFixed(2)}</Text>
+            <Text style={tw`mt-5 `}>IMC: {imc.toFixed(2)}</Text>
+            <Text style={tw`mt-3 font-bold text-slate-500`}>Situação: {result}</Text>
         </View>
     )
 }
