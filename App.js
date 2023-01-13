01e40ff446bc0240531276e578c0d16118230688
Av3z <willyanf17@hotmail.com>
2023-01-13 16:46:49 -0300

    chore: added path for calculator IMM

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import IMCScreen from './src/pages/IMCScreen';
+import IMMScreen from './src/pages/IMMScreen';
 import HomeScreen from './src/pages/HomeScreen';
 import WeightBoneScreen from './src/pages/WeightBoneScreen';
 import CalcAbdomen from './src/pages/CalcAbdomen';
@@ -18,6 +19,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen options={{ headerTitleAlign: 'center' }} name="Home" component={HomeScreen} />
         <Stack.Screen options={{ headerTitleAlign: 'center' }} name="IMC" component={IMCScreen} />
+        <Stack.Screen options={{ headerTitleAlign: 'center' }} name="IMM" component={IMMScreen} />
         <Stack.Screen options={{ headerTitleAlign: 'center', title: 'Peso Osseo' }} name="WeightBone" component={WeightBoneScreen} />
         <Stack.Screen options={{ headerTitleAlign: 'center', title: 'Perimetro do Abdômen' }} name="CalcAbdomen" component={CalcAbdomen} />
         <Stack.Screen options={{ headerTitleAlign: 'center', title: 'IAC' }} name="IAC" component={CalcIAC} />
