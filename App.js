8bdb67815ecffc1187085ed0c2fd2fc533fc65e8
Av3z <willyanf17@hotmail.com>
2023-01-12 21:53:18 -0300

    chore: add patch of calculator IGC

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import HomeScreen from './src/pages/HomeScreen';
 import WeightBoneScreen from './src/pages/WeightBoneScreen';
 import CalcAbdomen from './src/pages/CalcAbdomen';
 import CalcIAC from './src/pages/CalcIAC';
+import IGCScreen from './src/pages/IGCScreen';
 
 
 export default function App() {
@@ -20,6 +21,7 @@ export default function App() {
         <Stack.Screen options={{ headerTitleAlign: 'center', title: 'Peso Osseo' }} name="WeightBone" component={WeightBoneScreen} />
         <Stack.Screen options={{ headerTitleAlign: 'center', title: 'Perimetro do Abdômen' }} name="CalcAbdomen" component={CalcAbdomen} />
         <Stack.Screen options={{ headerTitleAlign: 'center', title: 'IAC' }} name="IAC" component={CalcIAC} />
+        <Stack.Screen options={{ headerTitleAlign: 'center', title: 'IGC' }} name="IGC" component={IGCScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
